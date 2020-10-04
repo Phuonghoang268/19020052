b3f237b36e8661e9cd86c45e1bec1358c3ff0be3
Phuonghoang268 <71497691+Phuonghoang268@users.noreply.github.com>
2020-10-04 09:01:37 +0700

    Update DictionaryCommanLine.java

--- src/DictionaryCommanLine.java
+++ src/DictionaryCommanLine.java
@@ -1,6 +1,12 @@
 public class DictionaryCommanLine {
     public static void dictionaryBasic() {
-        DictionaryManagement.insertFromCommandline();
+        DictionaryManagement.insertFromFile();
         DictionaryCommandline.showAllWords();
+    }
+
+    public static void dictionaryAdvanced() {
+        DictionaryManagement.insertFromFile();
+        DictionaryCommandline.showAllWords();
+        DictionaryManagement.dictionaryLookup();
     }
 }
