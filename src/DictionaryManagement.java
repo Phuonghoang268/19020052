ba76f80676c9f178104970616d7aa42b24f7f120
Phuonghoang268 <71497691+Phuonghoang268@users.noreply.github.com>
2020-10-04 08:58:16 +0700

    Update DictionaryManagement.java

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class DictionaryManagement {
     public static void insertFromCommandline() {
@@ -12,4 +14,34 @@ public class DictionaryManagement {
             Dictionary.addWord(temp);
         }
     }
+    public static void insertFromFile()  {
+        try{
+            File file= new File("dic1.txt");
+            Scanner scan = new Scanner(file);
+            int dem = scan.nextInt();
+            for (int i = 0; i < dem; i++) {
+                String w1 = scan.next();
+                String w2 = scan.nextLine();
+                w2=w2.replaceAll("\t","");
+                Word temp = new Word(w1, w2);
+                Dictionary.addWord(temp);
+            }
+        }
+        catch (FileNotFoundException e){
+        }
+    }
+
+    public static void dictionaryLookup(){
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        for(int i=0;i<Dictionary.count;i++){
+            if(s.equals(Dictionary.words[i].getWord_target())) {
+                System.out.println(Dictionary.words[i].getWord_explain());
+                break;
+            }
+        }
+    }
 }
+
+
