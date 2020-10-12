4427cc1e97ac6574159c269f08bb0dd51a134e37
Phuonghoang268 <71497691+Phuonghoang268@users.noreply.github.com>
2020-10-12 22:50:55 +0700

    Update DictionaryCommanLine.java

--- src/DictionaryCommanLine.java
+++ src/DictionaryCommanLine.java
@@ -1,12 +1,22 @@
-public class DictionaryCommanLine {
+import java.util.Scanner;
+
+public class DictionaryCommanLine  {
     public static void dictionaryBasic() {
         DictionaryManagement.insertFromFile();
         DictionaryCommandline.showAllWords();
     }
 
-    public static void dictionaryAdvanced() {
+    public static void dictionaryAdvanced(){
         DictionaryManagement.insertFromFile();
         DictionaryCommandline.showAllWords();
         DictionaryManagement.dictionaryLookup();
+    }
+
+    public static void dictionarySearcher(String search){
+            for(int i=0;i<Dictionary.count;i++){
+            if(Dictionary.words[i].getWord_target().toLowerCase().contains(search.toLowerCase())){
+                System.out.println(Dictionary.words[i].getWord_target());
+            }
+        }
     }
 }
